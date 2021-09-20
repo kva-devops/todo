f3d39798527d4f5a7a618c2cacec33b9c841deed
vladimirKutyavin <kva.devops@gmail.com>
2021-09-20 15:01:27 +0400

    3. Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/store/HbnStore.java
+++ src/main/java/ru/job4j/store/HbnStore.java
@@ -2,11 +2,13 @@ package ru.job4j.store;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.job4j.model.Item;
 import java.util.List;
+import java.util.function.Function;
 
 public class HbnStore implements Store, AutoCloseable {
 
@@ -16,13 +18,27 @@ public class HbnStore implements Store, AutoCloseable {
     private final SessionFactory sf = new MetadataSources(registry)
             .buildMetadata().buildSessionFactory();
 
-
     private static final class Lazy {
         private static final Store INST = new HbnStore();
     }
 
     public static Store instOf() {
         return Lazy.INST;
+    }
+
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 
     @Override
@@ -32,51 +48,34 @@ public class HbnStore implements Store, AutoCloseable {
 
     @Override
     public List<Item> findAllItemCheckOff() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List result = session.createQuery("from ru.job4j.model.Item where done=false order by id asc")
-                .list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(
+                session -> session.createQuery("from ru.job4j.model.Item where done=false order by id asc").list()
+        );
     }
 
     @Override
     public List<Item> findAllItemCheckOffAndCheckOn() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List result = session.createQuery("from ru.job4j.model.Item order by id asc ").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(
+            session -> session.createQuery("from ru.job4j.model.Item order by id asc").list()
+        );
     }
 
     @Override
     public void saveItem(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
+        this.tx(session -> session.save(item));
     }
 
     @Override
     public void updateItem(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        item.setDone(true);
-        session.update(item);
-        session.getTransaction().commit();
-        session.close();
+        this.tx(session -> {
+            item.setDone(true);
+            session.update(item);
+            return null;
+        });
     }
 
     @Override
     public Item findById(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item result = session.get(Item.class, id);
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(session -> session.get(Item.class, id));
     }
 }
